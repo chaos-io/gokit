bec2e3dbe6956db9366e35cad019805a7945bde1
Eric Yue <yuekewin@gmail.com>
2025-02-07 16:07:39 +0800

    feat(tracing): update jaeger Tracer

--- tracing/client.go
+++ tracing/client.go
@@ -3,31 +3,22 @@ package tracing
 import (
 	"context"
 
-	"go.opentelemetry.io/otel"
-
 	"github.com/chaos-io/chaos/logs"
+	"go.opentelemetry.io/otel/trace"
 )
 
-func New(name string) (func(context.Context) error, error) {
+func New(name string) (trace.Tracer, func(context.Context) error) {
 	return NewWith(name, NewConfig("tracing"))
 }
 
-func NewWith(name string, cfg *Config) (func(context.Context) error, error) {
+func NewWith(name string, cfg *Config) (trace.Tracer, func(context.Context) error) {
 	if cfg == nil {
 		logs.Fatal("failed to create the tracer coz of given nil config")
 		return nil, nil
 	}
 
 	if cfg.Enable {
-		ctx := context.Background()
-		tracerProvider, err := NewJaegerTraceProvider(ctx, name, cfg.Url)
-		if err != nil {
-			logs.Errorw("failed to create the tracing", "name", name, "host", cfg.Url, "error", err)
-			return nil, err
-		}
-
-		otel.SetTracerProvider(tracerProvider)
-		return tracerProvider.Shutdown, nil
+		return NewTracer(context.Background(), name, cfg.Url)
 	}
 
 	return nil, nil
--- tracing/jaeger.go
+++ tracing/jaeger.go
@@ -4,13 +4,27 @@ import (
 	"context"
 	"time"
 
+	"github.com/chaos-io/chaos/logs"
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/sdk/resource"
 	traceSDK "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"go.opentelemetry.io/otel/trace"
 )
 
-func NewJaegerTraceProvider(ctx context.Context, serviceName, jaegerEndpoint string) (*traceSDK.TracerProvider, error) {
+func NewTracer(ctx context.Context, serviceName, jaegerEndpoint string) (trace.Tracer, func(ctx context.Context) error) {
+	provider, err := NewTraceProvider(ctx, serviceName, jaegerEndpoint)
+	if err != nil {
+		logs.Fatalw("failed to create trace provider", "error", err)
+	}
+
+	otel.SetTracerProvider(provider)
+	tracer := provider.Tracer(serviceName)
+	return tracer, provider.Shutdown
+}
+
+func NewTraceProvider(ctx context.Context, serviceName, jaegerEndpoint string) (*traceSDK.TracerProvider, error) {
 	exp, err := otlptracehttp.New(ctx,
 		otlptracehttp.WithEndpoint(jaegerEndpoint),
 		otlptracehttp.WithInsecure())
